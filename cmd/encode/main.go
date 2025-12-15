1c732ca36bfe31204c2502e11e7b9c583a34a50b
Pieter De Gendt <pieter.degendt@basalte.be>
2025-12-15 18:43:49 +0100

    cmd: Add sentinel flag for decode/encode commands
    Introduce flags with short/long names for a custom sentinel value.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- cmd/encode/main.go
+++ cmd/encode/main.go
@@ -3,28 +3,47 @@ Encode reads from standard input, and writes the encoded data to standard output
 
 Usage:
 
-    encode [flags]
+	encode [flags]
 
 The flags are:
 
-    -del
-        Append the encoded data with a (zero) delimiter.
+	-d/-del
+	    Append the encoded data with the sentinel delimiter.
+
+	-s/-sentinel
+	    Use a custom sentinel value (default is 0x00).
 */
 package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"os"
 
 	"github.com/pdgendt/cobs"
 )
 
+var appendDelim bool
+var sentinel int
+
+func init() {
+	flag.BoolVar(&appendDelim, "del", false, "Append a delimiter")
+	flag.BoolVar(&appendDelim, "d", false, "Append a delimiter")
+	flag.IntVar(&sentinel, "sentinel", int(cobs.Delimiter), "Sentinel value (default is 0x00)")
+	flag.IntVar(&sentinel, "s", int(cobs.Delimiter), "Sentinel value (default is 0x00)")
+}
+
 func main() {
-	delimiter := flag.Bool("del", false, "Append a delimiter")
 	flag.Parse()
 
-	enc := cobs.NewEncoder(os.Stdout)
+	if sentinel < 0 || sentinel > 255 {
+		fmt.Fprintf(os.Stderr, "Error: sentinel value (%d) must be in [0x00, 0xFF]\n",
+			sentinel)
+		os.Exit(1)
+	}
+
+	enc := cobs.NewEncoder(os.Stdout, cobs.WithSentinel(byte(sentinel)))
 
 	if _, err := io.Copy(enc, os.Stdin); err != nil {
 		panic(err)
@@ -34,7 +53,7 @@ func main() {
 		panic(err)
 	}
 
-	if *delimiter {
-		os.Stdout.Write([]byte{cobs.Delimiter})
+	if appendDelim {
+		os.Stdout.Write([]byte{byte(sentinel)})
 	}
 }
