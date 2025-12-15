f4326e6cabd649d502d8795c965df6efc72a67bc
Pieter De Gendt <pieter.degendt@basalte.be>
2025-12-15 18:43:49 +0100

    cmd: Add sentinel flag for decode/encode commands
    Introduce flags with short/long names for a custom sentinel value.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- cmd/decode/main.go
+++ cmd/decode/main.go
@@ -3,22 +3,44 @@ Decode reads from standard input, and writes the decoded data to standard output
 
 Usage:
 
-    decode
+	decode [flags]
 
-When decode reads a zero delimiter it will stop processing data. If malformed encoded data
+The flags are:
+
+	-s/-sentinel
+	    Use a custom sentinel value (default is 0x00).
+
+When decode reads the sentinel delimiter it will stop processing data. If malformed encoded data
 is passed the program will panic.
 */
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"os"
 
 	"github.com/pdgendt/cobs"
 )
 
+var sentinel int
+
+func init() {
+	flag.IntVar(&sentinel, "sentinel", int(cobs.Delimiter), "Sentinel value (default is 0x00)")
+	flag.IntVar(&sentinel, "s", int(cobs.Delimiter), "Sentinel value (default is 0x00)")
+}
+
 func main() {
-	dec := cobs.NewDecoder(os.Stdout)
+	flag.Parse()
+
+	if sentinel < 0 || sentinel > 255 {
+		fmt.Fprintf(os.Stderr, "Error: sentinel value (%d) must be in [0x00, 0xFF]\n",
+			sentinel)
+		os.Exit(1)
+	}
+
+	dec := cobs.NewDecoder(os.Stdout, cobs.WithSentinel(byte(sentinel)))
 
 	if _, err := io.Copy(dec, os.Stdin); err != nil && err != cobs.EOD {
 		panic(err)
